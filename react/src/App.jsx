6aecd2d66288f16d75c2a3f88e532ff1d5e572da
matias.huhta@fcg.fi <Matias.Huhta@fcg.fi>
2022-08-10 13:53:58 +0300

    React example base

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  console.log("Hello world!");
+
+  return (
+    <div className="App">
+      <h2>Hello world!</h2>
+    </div>
+  )
+}
+
+export default App
