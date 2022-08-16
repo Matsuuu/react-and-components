ae98fd6e9175b4803d784ce06787edd11e44e8d0
Matsuuu <huhta.matias@gmail.com>
2022-08-16 21:49:09 +0300

    A new approach

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+
+const parts = [
+    { href: "/properties", title: "Properties" },
+    { href: "/change-detection", title: "Change detection" },
+    { href: "/expressions", title: "Expression" },
+    { href: "/conditional-rendering", title: "Conditional Rendering" },
+    { href: "/lists-and-arrays", title: "Lists and arrays" },
+    { href: "/nesting", title: "Nesting" },
+    { href: "/element-composition", title: "Composing elements" },
+    { href: "/part-composition", title: "Composing parts" },
+    { href: "/styling", title: "Styling components" },
+    { href: "/working-with-shadow-dom", title: "Working with shadow dom" },
+];
 
 function App() {
   console.log("Hello world!");
 
+    function renderDemo() {
+        const path = window.location.pathname;
+
+        switch(path) {
+
+            default:
+                return <></>
+        }
+    }
+
   return (
     <div className="App">
-      <h2>Hello world!</h2>
+      <h2>Web Components Workshop</h2>
+      <section>
+        {renderDemo()}
+
+          <ul>
+            {parts.map(part => <li key={part.href}><a href={part.href}>{part.title}</a></li>)}
+          </ul>
+      </section>
+
     </div>
   )
 }
