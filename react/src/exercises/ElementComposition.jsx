93d59a7a0fbed81ee15bd5b28f150bcacfe27a5e
Matsuuu <huhta.matias@gmail.com>
2022-08-17 21:43:27 +0300

    Element composition

--- react/src/exercises/ElementComposition.jsx
+++ react/src/exercises/ElementComposition.jsx
@@ -1,3 +1,45 @@
+import React from "react";
+
 export function ElementComposition() {
-    return <h3>ElementComposition</h3>
+    return (
+        <div>
+            <h3>ElementComposition</h3>
+
+            <div className="divider"></div>
+            
+            <ElementWithChildSlot>
+                <h3>Hello World!</h3>
+                <button>Boop</button>
+            </ElementWithChildSlot>
+
+            <div className="divider"></div>
+
+            <ElementWithNamedParts
+                top={<p><b>I'm at the top!</b></p>}
+                bottom={<p><b>I'm at the bottom!</b></p>}
+            />
+        </div>
+    );
+}
+
+export function ElementWithChildSlot(props) {
+
+    return (
+        <div>
+            <p>The content inserted will appear between this line</p>
+            {props.children}
+            <p>and this line</p>
+        </div>
+    )
+}
+
+export function ElementWithNamedParts(props) {
+
+    return (
+        <div>
+            {props.top}
+            <p>This content will be between two child element parts</p>
+            {props.bottom}
+        </div>
+    )
 }
