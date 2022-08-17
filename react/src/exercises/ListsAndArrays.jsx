058f1a1d3a720b492af85e4f82a1c00b6b4316fb
Matsuuu <huhta.matias@gmail.com>
2022-08-17 20:57:20 +0300

    Iterating items

--- react/src/exercises/ListsAndArrays.jsx
+++ react/src/exercises/ListsAndArrays.jsx
@@ -1,3 +1,33 @@
+import React from "react";
+
 export function ListsAndArrays() {
-    return <h3>Lists And Arrays</h3> 
+
+    const numbers = [...new Array(5)].map(() => Math.floor(Math.random() * 20));
+    console.log(numbers);
+
+    const languages = [
+        { name: "Javascript", short: "JS" },
+        { name: "Typescript", short: "TS" },
+        { name: "Java", short: "Java" },
+        { name: "Python", short: "Py" },
+    ]
+
+    const languageList = languages.map(lang => (
+        <li key={lang.short}>{lang.name} ({lang.short})</li>
+    ));
+
+    return (
+        <div>
+            <h3>Lists And Arrays</h3> 
+
+            <p>Numbers: </p>
+            <ul>
+                {numbers.map(num => <li key={num}>{num}</li>)}
+            </ul>
+
+            <ul>
+                {languageList}
+            </ul>
+        </div>
+    )
 }
