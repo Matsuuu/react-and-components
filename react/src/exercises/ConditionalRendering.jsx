8877cf3b7392a37d94906bbcb57aabda433bfbb4
Matsuuu <huhta.matias@gmail.com>
2022-08-17 20:45:02 +0300

    Conditionals

--- react/src/exercises/ConditionalRendering.jsx
+++ react/src/exercises/ConditionalRendering.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function ConditionalRendering() {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    let countMessage = <></>;
+    if (count > 10) {
+        countMessage = <p>Wow, that's over ten!</p>;
+    }
+
+    function renderMessage() {
+        if (count <= 10) return;
+        return <p>Wow, that's over ten!</p>;
+    }
+
     return (
         <div>
+            <h3>Conditional rendering</h3>
+            <p>Count: {count}</p>
 
+            {countMessage}
+
+            {count > 10 && <p>Wow, that's over ten!</p>}
+
+            {count > 10 ? <p>Wow, that's over ten!</p> : <></>}
+
+            {renderMessage()}
+            
+            <button onClick={increment}>Increment</button>
         </div>
     )
 }
