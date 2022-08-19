e78ab0d7b3b9cd2842869414ab1b9195b82c8244
Matsuuu <huhta.matias@gmail.com>
2022-08-19 15:06:24 +0300

    Adding styles

--- react/src/exercises/Styling.jsx
+++ react/src/exercises/Styling.jsx
@@ -1,3 +1,35 @@
+import React from "react";
+import "./Button.css";
+
+const redButtonStyle = {
+    color: "#FFF",
+    backgroundColor: "#EE4B2B",
+    padding: "0.4rem"
+}
+
 export function Styling() {
-    return <h3>Styling</h3>
+    return (
+        <div>
+            <h3>Styling</h3>
+
+            <button style={redButtonStyle}>
+                Red Button
+            </button>
+
+            <div className="divider"></div>
+
+            <button style={{ backgroundColor: "blue", color: "#FFF", padding: "0.4rem" }}>
+                Blue button
+            </button>
+
+            <div className="divider"></div>
+
+            <button className="green-button">
+                Green Button
+            </button>
+
+            <div className="divider"></div>
+
+        </div>
+    )
 }
